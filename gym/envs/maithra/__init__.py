6a85ff897ffbc150c838d937d6e496561822f052
Maithra <maithrar@gmail.com>
2018-04-15 21:45:35 +0000

    adding mulitagent enviroments to init

--- gym/envs/maithra/__init__.py
+++ gym/envs/maithra/__init__.py
@@ -1,3 +1,4 @@
 from gym.envs.maithra.erdosgame import ErdosGameEnv
 from gym.envs.maithra.erdosgameattacker import ErdosGameAttackerEnv
 from gym.envs.maithra.erdosgamecattacker import ErdosGameCAttackerEnv
+from gym.envs.maithra.erdosgame_smallK import ErdosGameSmallKEnv
